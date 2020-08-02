01007fbbf63d2aff7670d908dc450a97a6f50c73
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-02 16:05:03 +0300

    minor model change + test server route

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,8 @@ const { Schema, model, Types } = require('mongoose');
 
 const schema = new Schema({
     username:  {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}, 
-    routes: [{ type: Types.ObjectId, ref: 'Route' }]
+    user_routes: [{ type: Types.ObjectId, ref: 'Route' }],
+    saved_routes: [{ type: Types.ObjectId, ref: 'Route' }]
 });
 
 module.exports = model('User', schema);
