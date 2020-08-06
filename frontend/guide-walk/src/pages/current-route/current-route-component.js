08e6ffa2b5c217e8ad7c63a92a4f8b6994c1ca1a
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-06 21:39:38 +0300

    some first changes

---
+++ frontend/guide-walk/src/pages/current-route/current-route-component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CurrentRoute } from '../../ui/current-route-component';
+
+export const CurrentRouteComponent = () => {
+  return (
+    <div>    
+      <CurrentRoute />
+    </div>
+  );
+};
---
+++ frontend/guide-walk/src/pages/current-route/current-route-container.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = {};
+
+export const CurrentRouteContainer = connect(mapStateToProps, mapDispatchToProps);
