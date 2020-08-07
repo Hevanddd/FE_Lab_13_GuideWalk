f827bcfd3d6734001f9fcf213be586f7a03036ac
Hevanddd <mandzik123@gmail.com>
2020-08-08 02:06:18 +0300

    Add validation to form

--- frontend/guide-walk/src/ui/add-edit-point-form-component/add-edit-point-form-component.js
+++ frontend/guide-walk/src/ui/add-edit-point-form-component/add-edit-point-form-component.js
@@ -7,15 +7,15 @@ import { useForm, Controller } from 'react-hook-form';
 
 import styles from './add-edit-point-form.module.scss';
 
-const AddEditPointFormComponent = ({ savePoint, editedPoint }) => {
-  const { register, handleSubmit, control, setValue } = useForm();
+const AddEditPointFormComponent = ({ savePoint, editedPoint, titles}) => {
+  const { register, handleSubmit, control, setValue, setError, errors, clearErrors } = useForm();
 
   const title = editedPoint ? editedPoint.title : '';
   const description = editedPoint ? editedPoint.description : '';
   const initialCoords = editedPoint && editedPoint.location;
 
   const [coordinates, setCoordinates] = useState(initialCoords);
-
+  
   const submitPoint = (point) => {
     point.location = coordinates;
     if (editedPoint) {
@@ -29,22 +29,44 @@ const AddEditPointFormComponent = ({ savePoint, editedPoint }) => {
   useEffect(() => {
     setValue('title', title);
     setValue('description', description);
-  }, []);
+  }, [title, description]);
 
   useEffect(() => {
     setCoordinates(initialCoords);
   }, [initialCoords]);
+  
 
   return (
     <div styleName='form'>
       <TextField
-        inputRef={register}
+        inputRef={register({
+          required: true,
+          validate: {
+            occupied: (value) => {
+              let isOccupied;
+              titles.forEach(el => {
+                if (value === el){
+                  isOccupied = false;
+                }
+              })
+              return isOccupied;
+            }
+          }
+        })}
         name='title'
         label='Point Name'
         placeholder={'Enter title'}
         variant='outlined'
         InputLabelProps={{ shrink: true }}
       />
+
+      {errors.title && errors.title.type === 'required' && (
+      <p styleName='error'>Enter title of your point.</p>
+      )}
+    
+      {errors.title && errors.title.type === "occupied" && (
+        <p styleName='error'>You already have this point</p>
+      )}
 
       <Controller
         as={TextField}
@@ -56,7 +78,11 @@ const AddEditPointFormComponent = ({ savePoint, editedPoint }) => {
         placeholder='Enter description'
         variant='outlined'
         InputLabelProps={{ shrink: true }}
+        rules={{ required: true }}
       />
+
+      { errors.description && <p styleName='error'> Enter description about your point.</p> }
+
 
       <MapAddEditPoint
         width={'100%'}
