c92cfcb6135c66772a26c208e2f3cc0a458c8202
Hevanddd <mandzik123@gmail.com>
2020-08-11 18:45:46 +0300

    Fix add-preview

--- frontend/guide-walk/src/pages/preview-route/preview-route-component.js
+++ frontend/guide-walk/src/pages/preview-route/preview-route-component.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import CSSModules from 'react-css-modules';
 import Button from '@material-ui/core/Button';
-import { withRouter } from 'react-router';
+import { useHistory } from 'react-router';
 import { formatDate } from './helpers';
 import { MapDrawComponent } from '../../ui/map-draw';
 
 import styles from './preview.module.scss';
 
-const PreviewRouteComponent = ({getAllRouteDataStart, routeData, setCurrentRoute, currentRoute, location}) => {
-    
-  const routeId = location.search.replace(/\?/, '');
+const PreviewRouteComponent = ({getAllRouteDataStart, routeData, setCurrentRoute, currentRoute}) => {
+  const history = useHistory();
+  const routeId = history.location.search.replace(/\?/, '');
   const [route, setRoute] = useState(false);
 
   useEffect(() => {
     getAllRouteDataStart(routeId);
-  }, []);
+  }, [getAllRouteDataStart, routeId]);
   
   useEffect(() => {
     if(routeData){
@@ -22,7 +22,12 @@ const PreviewRouteComponent = ({getAllRouteDataStart, routeData, setCurrentRoute
     }
   }, [routeData]);
   
-  const {name, description, author, rating, creation_date, ownerName} = route.route;
+  const handleOnClick = () => {
+    setCurrentRoute(routeId);
+    history.push('/current-route');
+  };
+
+  const {name, description, rating, creation_date, ownerName} = route && route.route;
   const date = formatDate(creation_date);
   const {points} = route;
   const firstPoint = points && points[0].location;
@@ -32,35 +37,37 @@ const PreviewRouteComponent = ({getAllRouteDataStart, routeData, setCurrentRoute
 
   return (
       <>
-        <div styleName='preview'>
-          <p styleName='preview__name'>{name}</p>
-          <p styleName='preview__description'>{description}</p>
-          {currentRoute && errorMessage}
-          <Button styleName='preview__btn' type='button' color='primary' variant='contained' disabled = {!!currentRoute} onClick={() => setCurrentRoute(routeId)}>
-            Start This Route
-          </Button>
-          <div styleName='preview__wrapper'>
-            <div>
-              <p>Author</p>
-              <span>{ownerName}</span>
-            </div>
-            <div>
-              <p>Rating</p>
-              <span styleName='preview__rating'>
-                {rating}
-              </span>
-            </div>
-            <div>
-              <p>Date</p>
-              <span>{date}</span>
+      {route &&
+        <>
+          <div styleName='preview'>
+            <p styleName='preview__name'>{name}</p>
+            <p styleName='preview__description'>{description}</p>
+            {currentRoute && errorMessage}
+            <Button styleName='preview__btn' type='button' color='primary' variant='contained' disabled = {!!currentRoute} onClick={handleOnClick}>
+              Start This Route
+            </Button>
+            <div styleName='preview__wrapper'>
+              <div>
+                <p>Author</p>
+                <span>{ownerName}</span>
+              </div>
+              <div>
+                <p>Rating</p>
+                <span styleName='preview__rating'>
+                  {rating}
+                </span>
+              </div>
+              <div>
+                <p>Date</p>
+                <span>{date}</span>
+              </div>
             </div>
           </div>
-        </div>
-        <MapDrawComponent styleName='map' firstPoint={firstPoint} lastPoint={lastPoint} />
+          <MapDrawComponent styleName='map' firstPoint={firstPoint} lastPoint={lastPoint} />
+        </>
+      }
     </>
   );
 };
-
-const PreviewRoute = withRouter(PreviewRouteComponent);
 
 export default CSSModules(PreviewRouteComponent, styles);
