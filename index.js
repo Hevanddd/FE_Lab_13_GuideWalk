bf946db97e03125d9cf43aa857a03dfa2cbcfdd3
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-14 01:27:13 +0300

    finally?

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-
 const app = express();
+
 app.use(express.json({ extended: true }));
 
+const PORT = process.env.PORT || config.get("port") || 5000;
+
+app.use(express.static("frontend/guide-walk/build"));
 
 app.use("/api/route", require("./routes/route.routes"));
 app.use("/api/user", require("./routes/user.routes"));
 
-const PORT = process.env.PORT || config.get("port") || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(path.join(__dirname, 'frontend/guide-walk/build'));
-}
-
 async function start() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || config.get("mongoUri") || 5000 , {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URI || config.get("mongoUri") || 5000,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
     app.listen(PORT, () => {
       console.log(`App has been started on port ${PORT}...`);
     });
