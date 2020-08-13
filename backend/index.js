7feff745434aed9a742af43132e3ff8021699f98
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-13 22:06:22 +0300

    pushing to heroku

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,23 @@ const config = require("config");
 const app = express();
 app.use(express.json({ extended: true }));
 
+app.get("", async (req, res) => {
+  res.json({ message: "im here" });
+});
+
 app.use("/api/route", require("./routes/route.routes"));
 app.use("/api/user", require("./routes/user.routes"));
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || config.get("port") || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get("mongoUri"), {
+    await mongoose.connect(process.env.MONGODB_URI || config.get("mongoUri"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log(`App has been started on port ${PORT}...`);
     });
   } catch (e) {
