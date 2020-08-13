d703f18308c2918ecd5212d041bc1829a1acf6df
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-14 01:34:40 +0300

    one more time?

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 
+
 const PORT = process.env.PORT || config.get("port") || 5000;
 
 app.use(express.static("frontend/guide-walk/build"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/guide-walk/build', 'index.html'))
+})
 
 app.use("/api/route", require("./routes/route.routes"));
 app.use("/api/user", require("./routes/user.routes"));
