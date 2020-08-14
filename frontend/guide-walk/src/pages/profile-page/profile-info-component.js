5b89f261b6438bac70590fbdb94f1c06654ac6fc
Hevanddd <mandzik123@gmail.com>
2020-08-14 14:56:05 +0300

    Add profile img

--- frontend/guide-walk/src/pages/profile-page/profile-info-component.js
+++ frontend/guide-walk/src/pages/profile-page/profile-info-component.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { TextField }  from '@material-ui/core'; 
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { withStyles } from '@material-ui/core/styles';
 
 import styles from './profile-info-component.module.scss';
@@ -28,14 +26,13 @@ export const ProfileInfoComponent = ({userAuthData}) => {
   const {isAuthenticated, logout} = useAuth0();
   const userName = userAuthData && userAuthData.userName;
   const userEmail = userAuthData && userAuthData.email;
+  const imgUrl = userAuthData && userAuthData.picture;
 
   return (
     <div className={styles.form}>           
       <form className={styles.form}>
         <div className={styles.form__img}>
-          <IconButton aria-label="account circle" disabled={true}>
-            <AccountCircleIcon  className={styles.form__img__large}/>
-          </IconButton>
+          <img src={imgUrl} alt={userName} />
         </div>
         <CssTextField
           label="Name"
