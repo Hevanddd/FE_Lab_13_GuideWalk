91610c0167ccb9289e9b256448c6ecfc13753e0c
Yaryna Brylova <yaryna.bryliova@gmail.com>
2020-08-17 17:22:09 +0300

    fix warning

--- frontend/guide-walk/src/pages/tab-routes/tab-routes-component.js
+++ frontend/guide-walk/src/pages/tab-routes/tab-routes-component.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
