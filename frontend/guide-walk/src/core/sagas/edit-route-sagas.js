5f35d099acb38cb0a1d5a78dfc9f8e561132e284
Roman Koshla <55313764+Ramon049@users.noreply.github.com>
2020-08-17 11:36:57 +0300

    Fix/map marker (#35)
    * Fix style
    
    * Click on map added marker

--- frontend/guide-walk/src/core/sagas/edit-route-sagas.js
+++ frontend/guide-walk/src/core/sagas/edit-route-sagas.js
@@ -1,13 +1,7 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
 import { httpRequest } from '../../services';
 import { toast } from 'react-toastify';
-import {
-  editRouteStart,
-  editRouteFail,
-  editRouteSuccess,
-  loadingStop,
-  loadingStart,
-} from '../redux/actions';
+import { editRouteStart, editRouteFail, editRouteSuccess, loadingStop, loadingStart } from '../redux/actions';
 import { handleErrorInSagas } from '../../services/helpers/handle-error-in-sagas';
 
 function* callEditRoute({ payload }) {
@@ -16,7 +10,6 @@ function* callEditRoute({ payload }) {
     const data = yield call(httpRequest, `/api/route/edit`, 'POST', payload);
     yield put(editRouteSuccess(data));
     yield call(toast.success, 'Route successfully added!', { autoClose: 2000 });
-    console.log('All is good');
   } catch (e) {
     yield call(handleErrorInSagas, editRouteFail);
   } finally {
