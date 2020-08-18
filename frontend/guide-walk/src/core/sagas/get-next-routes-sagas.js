699cd23c8d24e04397f1b1cf64dcc5f458641569
Roman Koshla <koshla.roman10@gmail.com>
2020-08-18 17:45:33 +0300

    Add paginations loader

--- frontend/guide-walk/src/core/sagas/get-next-routes-sagas.js
+++ frontend/guide-walk/src/core/sagas/get-next-routes-sagas.js
@@ -1,18 +1,15 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
 import { httpRequest } from '../../services';
-import { getNextRoutesStart, getNextRoutesFail, getNextRoutesSuccess, loadingStop, loadingStart } from '../redux/actions';
+import { getNextRoutesStart, getNextRoutesFail, getNextRoutesSuccess } from '../redux/actions';
 import { handleErrorInSagas } from '../../services/helpers/handle-error-in-sagas';
 
 function* callNextRoutesData({payload}) {
   try {
     const size = 10;
-    yield put(loadingStart());
     const data = yield call(httpRequest, `/api/route/?page=${payload}&size=${size}`, 'GET');
     yield put(getNextRoutesSuccess(data));
   } catch (e) {
     yield call(handleErrorInSagas, getNextRoutesFail);
-  } finally {
-    yield put(loadingStop());
   }
 }
 
