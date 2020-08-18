acf92e7d9e5d44e0f28c15e6158ece7839a6bd21
Mykhailo Benych <62563220+bnchhh@users.noreply.github.com>
2020-08-18 21:16:55 +0300

    to build, nothing changed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 
-
 app.use("/api/route", require("./routes/route.routes"));
 app.use("/api/user", require("./routes/user.routes"));
 
